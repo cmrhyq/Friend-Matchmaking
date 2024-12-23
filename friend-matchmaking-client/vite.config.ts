f1b8122a33521ed2051762d497b535c3e169de26
AlanHuang <63389034+cmrhyq@users.noreply.github.com>
2024-12-23 16:09:54 +0800

    1.配置前端vite.config配置

--- friend-matchmaking-client/vite.config.ts
+++ friend-matchmaking-client/vite.config.ts
@@ -6,6 +6,16 @@ import { VantResolver } from '@vant/auto-import-resolver';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // 项目构建过程配置
+  build: {
+    // 打包输出目录
+    outDir: 'dist',
+    // 是否压缩代码
+    minify: false,
+    // 是否生成sourcemap
+    sourcemap: true,
+  },
+
   plugins: [
     vue(),
     AutoImport({
@@ -15,4 +25,25 @@ export default defineConfig({
       resolvers: [VantResolver()],
     }),
   ],
+  // 配置服务端
+  server: {
+    // 端口号
+    port: 4000,
+    // 是否自动打开浏览器
+    open: true,
+    // 是否开启https
+    https: false,
+    // 监听所有地址
+    host: true,
+    // 跨域代理，为开发服务器配置CORS
+    cors: true,
+    // 代理配置
+    proxy: {
+      // '/api': {
+      //     target: 'URL_ADDRESS',
+      //     changeOrigin: true,
+      //     rewrite: (path) => path.replace(/^\/api/, ''),
+      // },
+    },
+  },
 })
